67b44a358a4ef15539688628e1ac6ff04d7cba05
ugabb <gsb.alternativo@gmail.com>
2023-10-27 16:46:20 -0300

    feat: Adicionando validacoes com Zod ao Schema

---
+++ backend-taugor-gestao-de-pessoas/src/models/FuncionarioSchema.ts
@@ -0,0 +1,41 @@
+import { z } from 'zod';
+
+const AddressSchema = z.object({
+  cep: z.string(),
+  logradouro: z.string(),
+  number: z.number(),
+  uf: z.string(),
+});
+
+const BirthdaySchema = z.object({
+  day: z.number(),
+  month: z.number(),
+  year: z.number(),
+});
+
+const AdmissioDateSchema = z.object({
+  day: z.number(),
+  month: z.number(),
+  year: z.number(),
+});
+
+const ContatoInfoSchema = z.object({
+  name: z.string(),
+  gender: z.string(),
+  address: AddressSchema,
+  phone: z.string(),
+  profilePicture: z.string(),
+  birthday: BirthdaySchema,
+});
+
+const FuncionarioInfoSchema = z.object({
+  role: z.string(),
+  admissioDate: AdmissioDateSchema,
+  sector: z.string(),
+  salary: z.number(),
+});
+
+export const FuncionarioSchema = z.object({
+  contatoInfo: ContatoInfoSchema,
+  funcionarioInfo: FuncionarioInfoSchema,
+});
