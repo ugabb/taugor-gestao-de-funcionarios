ac8919a8b088e64af36f9994449127f9065ccc4b
ugabb <gsb.alternativo@gmail.com>
2023-10-28 09:31:32 -0300

    refact: limpando index

--- backend-taugor-gestao-de-pessoas/src/index.ts
+++ backend-taugor-gestao-de-pessoas/src/index.ts
@@ -1,7 +1,5 @@
 import express, { Request, Response } from "express";
 import router from './routes/FuncionarioRoutes'
-import admin from 'firebase-admin'
-import { Funcionario } from "./models/FuncionarioSchema";
 
 const PORT = process.env.PORT || 8080;
 
@@ -9,49 +7,7 @@ const app = express();
 
 app.use(express.json())
 
-// router should be here
 app.use('/api/funcionario',router)
-
-// var serviceAccount = require("../serviceAccountKey.json");
-
-// admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount)
-// });
-
-// const db = admin.firestore();
-
-// GET -> /api/funcionario
-app.get('/api/funcionario', (req: Request, res: Response) => {
-    console.log("GET")
-});
-
-// GET by ID -> /api/funcionario/:id
-app.get('/api/funcionario', async (req: Request, res: Response) => {
-
-});
-
-// // POST -> /api/funcionario
-// app.post('/api/funcionario', async (req: Request, res: Response) => {
-//     try {
-//         const { name, phone } = req.body;
-
-//         // create a validation for the schema
-//         const funcionarioJson = {
-//             name,
-//             phone,
-
-//         }
-//         const response = await db.collection("funcionario").add(funcionarioJson)
-
-//         return res.status(201).json({ message: "Funcionario criado com sucesso", response })
-//     } catch (error) {
-//         console.log('Error adding document: ', error)
-//         return res.status(500).json({ error: "Falha ao criar o funcionário" })
-//     }
-// })
-
-// PUT -> /api/funcionario/:id
-// DELETE -> /api/funcionario/:id
 
 app.listen(PORT, () => {
     console.log(`Servidor iniciado na porta: ${PORT}`);
