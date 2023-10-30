c24134cf229491ee76559e24e1349fc93201de31
Gabriel Silva Barros <08175460148@mma.gov.br>
2023-10-30 16:48:40 -0300

    feat: middleware cors adicionado

--- backend-taugor-gestao-de-pessoas/src/index.ts
+++ backend-taugor-gestao-de-pessoas/src/index.ts
@@ -1,13 +1,15 @@
 import express, { Request, Response } from "express";
 import router from './routes/FuncionarioRoutes'
+import cors from 'cors'
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(express.json())
+app.use(cors())
 
-app.use('/api/funcionario',router)
+app.use('/api/funcionario', router)
 
 app.listen(PORT, () => {
     console.log(`Servidor iniciado na porta: ${PORT}`);
