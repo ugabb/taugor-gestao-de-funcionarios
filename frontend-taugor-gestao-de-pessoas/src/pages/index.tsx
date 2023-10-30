ea80c5a3d9de9abb3fbde37ce26ee15f8410130c
ugabb <gsb.alternativo@gmail.com>
2023-10-29 21:34:47 -0300

    feat: adicionando algumas rotas

--- frontend-taugor-gestao-de-pessoas/src/pages/index.tsx
+++ frontend-taugor-gestao-de-pessoas/src/pages/index.tsx
@@ -1,13 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Header from '@/components/Header/FormHeader'
 
-const inter = Inter({ subsets: ['latin'] })
+import { RiPencilFill } from 'react-icons/ri'
+import FormHeader from '@/components/Header/FormHeader'
 
 export default function Home() {
   return (
-    <>
-    
-    </>
+    <div>
+    Home
+    </div>
   )
 }
