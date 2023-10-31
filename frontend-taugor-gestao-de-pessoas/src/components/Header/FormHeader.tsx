17de438d2c2b5ee69e46069703eb4c08352ff975
Gabriel Silva Barros <08175460148@mma.gov.br>
2023-10-31 11:29:03 -0300

    feat: novos estilos para desktop

--- frontend-taugor-gestao-de-pessoas/src/components/Header/FormHeader.tsx
+++ frontend-taugor-gestao-de-pessoas/src/components/Header/FormHeader.tsx
@@ -5,17 +5,17 @@ import { IoMdHome } from 'react-icons/io'
 
 const Header = () => {
     return (
-        <header className='flex flex-col items-center justify-between'>
-            <div className='flex items-center flex-col'>
-                <Image src={'/marca-taugor.png'} alt='logo Taugor' width={309} height={109} />
+        <header className='flex flex-col lg:flex-row items-center justify-between '>
+            <div className='flex items-center flex-col lg:flex-row '>
+                <Image src={'/marca-taugor.png'} alt='logo Taugor' width={180} height={109} className='border-r' />
 
-                <div>
-                    <p className='text-gray-400'>PASSO 2 DE 6</p>
+                <div className='ml-10'>
+                    <p className='text-gray-400 text-xs'>PASSO 2 DE 6</p>
                     <h3 className='text-lg font-semibold'>Informação de contato</h3>
                 </div>
             </div>
 
-            <div className="hidden text-xl text-gray-400 p-5 border-r lg:block">
+            <div className="hidden text-xl text-gray-400 p-5 border-l lg:block">
                 <IoMdHome />
             </div>
 
