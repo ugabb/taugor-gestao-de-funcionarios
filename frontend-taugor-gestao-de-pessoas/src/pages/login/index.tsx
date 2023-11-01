08ed12fcc26377e9bcaca791ea7ae00f56344a1c
Gabriel Silva Barros <08175460148@mma.gov.br>
2023-11-01 14:19:18 -0300

    feat: link para Sign Up

--- frontend-taugor-gestao-de-pessoas/src/pages/login/index.tsx
+++ frontend-taugor-gestao-de-pessoas/src/pages/login/index.tsx
@@ -3,6 +3,7 @@
 import { auth } from '../../firebase'
 import { Button, TextField } from '@mui/material'
 import { signInWithEmailAndPassword } from 'firebase/auth'
+import Link from 'next/link'
 // next
 import { useRouter } from 'next/navigation'
 import { redirect } from 'next/navigation'
@@ -29,7 +30,7 @@ const Login = () => {
   return (
     <div className='flex flex-col justify-center items-center gap-5 h-screen'>
       <h1 className='text-3xl font-semibold text-primaryColor'>Log In</h1>
-      <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-3 w-2/3 lg:w-1/4'>
+      <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-3 w-4/5 md:w-1/3 lg:w-1/4'>
         <TextField
           inputProps={{
             className: 'bg-gray-50 rounded w-full',
@@ -55,6 +56,10 @@ const Login = () => {
 
         <Button variant="outlined" type='submit'>Log In</Button>
       </form>
+      <div className='text-gray-400 '>
+        ou
+        <Link href={'/sign-up'}><Button>Sign Up</Button></Link>
+      </div>
     </div>
   )
 }
