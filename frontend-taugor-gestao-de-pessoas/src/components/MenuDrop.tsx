28b5d9f4f16de014e03a1fcda5b030b2af4912b4
ugabb <gsb.alternativo@gmail.com>
2023-11-03 11:23:22 -0300

    feat: excluir funcionario

--- frontend-taugor-gestao-de-pessoas/src/components/MenuDrop.tsx
+++ frontend-taugor-gestao-de-pessoas/src/components/MenuDrop.tsx
@@ -4,6 +4,8 @@ import MenuItem from '@mui/material/MenuItem';
 import { useState } from 'react';
 import ModalUpdateFuncionario from './Modals/ModalUpdateFuncionario';
 import axios from 'axios';
+import { BsTrash } from 'react-icons/bs';
+import { useRouter } from 'next/router';
 
 
 type Props = {
@@ -32,9 +34,24 @@ const MenuDrop = ({ anchorEl, open, handleClose, handleClick, funcionarioID }: P
         handleClose()
     };
 
+    const router = useRouter()
+
     const demitirFuncionario = async () => {
         try {
             const response = await axios.patch(`${process.env.NEXT_PUBLIC_API_KEY}/fire/${funcionarioID}`)
+            alert("Demitido com sucesso!")
+            router.reload()
+            return response
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const excluirFuncionário = async () => {
+        try {
+            const response = await axios.delete(`${process.env.NEXT_PUBLIC_API_KEY}/${funcionarioID}`)
+            alert("Excluído com sucesso!")
+            router.reload()
             return response
         } catch (error) {
             console.log(error)
@@ -55,6 +72,7 @@ const MenuDrop = ({ anchorEl, open, handleClose, handleClick, funcionarioID }: P
             <MenuItem onClick={() => handleOpen(actions.atualizar)}>Atualizar dados</MenuItem>
             <MenuItem onClick={() => handleOpen(actions.promover)}>Promover Funcinário</MenuItem>
             <MenuItem onClick={demitirFuncionario}>Terminar Contrato</MenuItem>
+            <MenuItem onClick={excluirFuncionário}>Excluir Funcionário <BsTrash className="text-red-500 ml-3" /></MenuItem>
         </Menu>
     )
 }
