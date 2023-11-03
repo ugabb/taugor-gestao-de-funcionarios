33c91e10a4b92e16b7b041cfaa73352530c19a95
ugabb <gsb.alternativo@gmail.com>
2023-11-02 22:52:40 -0300

    feat: navbar dinamica

--- frontend-taugor-gestao-de-pessoas/src/components/Header/Header.tsx
+++ frontend-taugor-gestao-de-pessoas/src/components/Header/Header.tsx
@@ -1,12 +1,37 @@
-import { auth } from '@/firebase'
+
 import { Button } from '@mui/material'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+
+import { User, onAuthStateChanged } from 'firebase/auth';
+import { auth } from '@/firebase';
 
 import { IoMdHome } from 'react-icons/io'
 
 const Header = () => {
+    const [authUser, setAuthUser] = useState<User | null>(null); // Inicializando com tipo 'User | null'
+    const [isLoading, setIsLoading] = useState(true);
+
+
+    useEffect(() => {
+        const listen = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setAuthUser(user)
+                setIsLoading(false)
+            } else {
+                setAuthUser(null);
+            }
+        })
+        return () => {
+            listen()
+        }
+    }, [])
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
     const handleSignOut = async () => {
         try {
             await auth.signOut();
@@ -18,28 +43,31 @@ const Header = () => {
     };
 
     return (
-        <header className='flex flex-col lg:flex-row items-center justify-between shadow-sm'>
+        <header className='flex flex-col md:flex-row items-center justify-between shadow-sm'>
             <div className='flex items-center flex-col lg:flex-row '>
-                <Image src={'/marca-taugor.png'} alt='logo Taugor' width={180} height={109} />
+                <Link href={'/'} className='hover:scale-105 transition-all'>
+                    <Image src={'/marca-taugor.png'} alt='logo Taugor' width={180} height={109} />
+                </Link>
             </div>
 
             <div className='flex justify-center items-center gap-5'>
-                <Button>
-                    <Link href={"/cadastrar-funcionario"}>Cadastrar Funcinário</Link>
-                </Button>
-                <Button>
-                    <Link href={"/listar-funcionarios"}>Listar Funcinário</Link>
-                </Button>
-                <Button>
-                    <Link href={"/login"}>Sign In</Link>
-                </Button>
-                <Button onClick={handleSignOut}>
-                    <Link href={"/"} >Sign Out</Link>
-                </Button>
+                {authUser &&
+                    <>
+                        <Button className='hover:scale-105 transition-all'>
+                            <Link href={"/cadastrar-funcionario"}>Cadastrar Funcinário</Link>
+                        </Button>
+                        <Button className='hover:scale-105 transition-all'>
+                            <Link href={"/listar-funcionarios"}>Listar Funcinário</Link>
+                        </Button>
+                        <Button className='hover:scale-105 transition-all' onClick={handleSignOut}>
+                            <Link href={"/"} >Sign Out</Link>
+                        </Button>
+                    </>
+                }
             </div>
 
-            <Link href={"/"} className="hidden text-xl text-gray-400 p-5 lg:block">
-                <IoMdHome className="cursor-pointer transition-all hover:text-primaryColor" />
+            <Link href={"/"} className="hidden text-xl text-gray-400 p-5 md:block">
+                <IoMdHome className="cursor-pointer transition-all hover:text-primaryColor hover:scale-105" />
             </Link>
 
         </header>
