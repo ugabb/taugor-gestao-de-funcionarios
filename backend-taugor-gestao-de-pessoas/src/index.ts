4175816f1ba03f38023c28ff3cbf72600db55b03
ugabb <gsb.alternativo@gmail.com>
2023-11-02 23:02:07 -0300

    feat: dotenv variables

--- backend-taugor-gestao-de-pessoas/src/index.ts
+++ backend-taugor-gestao-de-pessoas/src/index.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from "express";
 import router from './routes/FuncionarioRoutes'
 import cors from 'cors'
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const PORT = process.env.PORT || 8080;
 
